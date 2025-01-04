927da09e3af121b7f301c4d42b1ba074ec02a687
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-01-04 15:31:50 -0300

    projeto 2 finalizado

--- pacotes/Projeto2/index.js
+++ pacotes/Projeto2/index.js
@@ -1,12 +1,28 @@
 import pensador from "pensador-scrap";
+import fs from "fs";
 
+
+const autor = "Jesus";
 const {error, success} = await pensador.search({
-    query: "Platao", 
+    query: autor, 
     limit: 12
 });
 
+let pensamentoPraSalvar;
+
+try {
 const pensamentos = success.thought;
 const indiceAleatorio = Math.floor(Math.random() * pensamentos.length);
 const pensamentoSelecionado = pensamentos[indiceAleatorio].content;
 
-console.log(pensamentoSelecionado);
+pensamentoPraSalvar = `Pensamento de ${autor} -> ${pensamentoSelecionado}\n`
+} catch (e) {
+    pensamentoPraSalvar = `${autor} -> não tem pensamentos \n`;
+} finally {
+// fs.writeFileSync("frases,txt", pensamentoPraSalvar, "utf-8");
+fs.appendFileSync("frases.txt", pensamentoPraSalvar, "utf-8");
+
+const texto = fs.readFileSync("frases.txt", "utf-8");
+console.log(texto)
+
+}
